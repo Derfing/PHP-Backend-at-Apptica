5d0bcdf640124c527ac925cd0505956d99a24a8a
Derfing <derendaevkosts45@gmail.com>
2025-09-16 18:33:17 +0300

    Add throttle api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,6 @@ use App\Http\Middleware\LogRequests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware([LogRequests::class])->group(function () {
+Route::middleware([LogRequests::class, 'throttle:5,1'])->group(function () {
     Route::get('/appTopCategory', [App\Http\Controllers\AppTopController::class, 'index']);
 });
